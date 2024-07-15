0467e2205d5cfbfa9034eca8819cbd3528161d34
William Grosset <williamhgrosset@gmail.com>
2024-07-14 23:57:15 -0600

    Add Lazy plugin manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,31 @@
+-- Plugin Manager
+require('config.lazy')
+
+-- Appearance
+vim.cmd('syntax enable')
+vim.cmd[[colorscheme tokyonight-night]]
+vim.opt.number = true
+vim.opt.ruler = true
+
+-- Format
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- Wrap
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.list = false
+
+-- Fold
+vim.opt.foldenable = true
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevelstart = 20
+
+-- Search
+vim.opt.hlsearch = true
+
 -- Mappings
 vim.keymap.set('n', '<space>', 'za')
 vim.keymap.set('n', ';', ':')
