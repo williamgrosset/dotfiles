6c0b4aeefbd43c6fcd124589d956591b8593c782
William Grosset <williamhgrosset@gmail.com>
2024-07-16 00:14:36 -0600

    Clean up script

--- scripts/copy.fish
+++ scripts/copy.fish
@@ -1,17 +1,17 @@
 #!/usr/bin/env fish
 
 set WEZTERM_SOURCE "$HOME/.config/wezterm"
-set FISH_CONFIG_SOURCE "$HOME/.config/fish/config.fish"
 set NVIM_SOURCE "$HOME/.config/nvim"
 set STARSHIP_SOURCE "$HOME/.config/starship"
 set GIT_SOURCE "$HOME/.config/git"
+set FISH_CONFIG_SOURCE "$HOME/.config/fish/config.fish"
 
 set TARGET_DIR (pwd)"/.config"
 
 cp -r $WEZTERM_SOURCE $TARGET_DIR
-cp $FISH_CONFIG_SOURCE $TARGET_DIR/fish
 cp -r $NVIM_SOURCE $TARGET_DIR
 cp -r $STARSHIP_SOURCE $TARGET_DIR
 cp -r $GIT_SOURCE $TARGET_DIR
+cp $FISH_CONFIG_SOURCE $TARGET_DIR/fish
 
 echo "Dotfiles have been copied to $TARGET_DIR"
