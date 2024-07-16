3ca535a965adacb4acc3a4da8c037ee5e1779fb6
William Grosset <williamhgrosset@gmail.com>
2024-07-16 12:09:28 -0600

    Add comment, gitsigns plugins; update treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  opts = function()
-    require"nvim-treesitter.configs".setup {
-      highlight = {
-        enable = true,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = function()
+      require"nvim-treesitter.configs".setup {
+        highlight = {
+          enable = true,
+        }
       }
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter"
     }
-  end
+  }
 }
