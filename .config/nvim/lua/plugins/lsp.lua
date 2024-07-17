6be00df2dc2d5f7e1826eaf8d2810d2e9e876c42
William Grosset <williamhgrosset@gmail.com>
2024-07-16 23:08:03 -0600

    Use luasnip > vsnip plugin

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -56,34 +56,32 @@ return {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-vsnip',
-      'hrsh7th/vim-vsnip'
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip'
     },
     config = function()
       local cmp = require'cmp'
+      local luasnip = require'luasnip'
 
       cmp.setup({
         snippet = {
           expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end
+            luasnip.lsp_expand(args.body)
+          end,
         },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true })
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
         }),
-        sources = cmp.config.sources(
-          {
-            { name = 'nvim_lsp' },
-            { name = 'vsnip' }
-          },
-          {
-            { name = 'buffer' }
-          }
-        )
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        })
       })
 
       -- Use buffer source for `/` and `?`
