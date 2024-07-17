d33386c465bf846e745666c9304d0671fb5f3440
William Grosset <williamhgrosset@gmail.com>
2024-07-17 17:51:26 -0600

    Disable font ligatures

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'tokyonight_night'
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.font = wezterm.font({
+  family = 'FiraCode Nerd Font',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+})
 config.font_size = 14.0
 
 return config
