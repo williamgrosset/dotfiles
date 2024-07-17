b6c0b05e4a504bb34f31e0b5a6dea830943f6aff
William Grosset <williamhgrosset@gmail.com>
2024-07-16 18:02:42 -0600

    Add Bat config

--- scripts/copy.fish
+++ scripts/copy.fish
@@ -5,6 +5,7 @@ set NVIM_SOURCE "$HOME/.config/nvim"
 set STARSHIP_SOURCE "$HOME/.config/starship"
 set GIT_SOURCE "$HOME/.config/git"
 set FISH_CONFIG_SOURCE "$HOME/.config/fish/config.fish"
+set BAT_CONFIG_SOURCE "$HOME/.config/bat/config"
 
 set TARGET_DIR (pwd)"/.config"
 
@@ -13,5 +14,6 @@ cp -r $NVIM_SOURCE $TARGET_DIR
 cp -r $STARSHIP_SOURCE $TARGET_DIR
 cp -r $GIT_SOURCE $TARGET_DIR
 cp $FISH_CONFIG_SOURCE $TARGET_DIR/fish
+cp $BAT_CONFIG_SOURCE $TARGET_DIR/bat
 
 echo "Dotfiles have been copied to $TARGET_DIR"
