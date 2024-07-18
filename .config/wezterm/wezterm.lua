9b16267832c604b1f87a97b9a055bd3983e964e7
William Grosset <williamhgrosset@gmail.com>
2024-07-17 21:17:45 -0600

    Use underline cursor and disable audio bell in Wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,20 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Color scheme
 config.color_scheme = 'tokyonight_night'
+
+-- Font
 config.font = wezterm.font({
   family = 'FiraCode Nerd Font',
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 })
 config.font_size = 14.0
+
+-- Cursor
+config.default_cursor_style = "BlinkingUnderline"
+
+-- Audio
+config.audible_bell = 'Disabled'
 
 return config
