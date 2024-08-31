7290da52324ba9b6601c5791b6f233906a67900a
William Grosset <williamhgrosset@gmail.com>
2024-08-30 23:42:48 -0700

    Use relative file path in Lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -24,7 +24,7 @@ return {
       sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_c = {{ 'filename', path = 1 }},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
@@ -32,7 +32,7 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
+        lualine_c = {{ 'filename', path = 1 }},
         lualine_x = {'location'},
         lualine_y = {},
         lualine_z = {}
@@ -44,3 +44,4 @@ return {
     })
   end
 }
+
