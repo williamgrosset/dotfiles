9427271c4e1daa63150bb53ae889e2708bf506f3
William Grosset <williamhgrosset@gmail.com>
2024-08-30 22:43:25 -0700

    Add Fzf and Nvimtree mappings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,20 @@
-vim.keymap.set('n', '<space>', 'za')
+-- Command mode
 vim.keymap.set('n', ';', ':')
 vim.keymap.set('n', ':', ';')
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
+
+-- Insert mode
 vim.keymap.set('i', 'jk', '<ESC>')
 vim.keymap.set('i', 'kj', '<ESC>')
+
+-- Navigation
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+
+-- Fold
+vim.keymap.set('n', '<space>', 'za')
+
+-- Fzf
+vim.keymap.set('n', '<c-P>', require('fzf-lua').live_grep, { desc = 'Fzf Files' })
+
+-- NvimTree
+vim.keymap.set('n', '<space>p', ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
