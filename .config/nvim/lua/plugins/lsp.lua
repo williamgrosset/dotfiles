790fa49df9b78a7dfc622f700aef8690961f5371
William Grosset <williamhgrosset@gmail.com>
2025-01-02 23:38:56 -0700

    Add lsp lines

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -109,5 +109,14 @@ return {
         )
       })
     end
+  },
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+      vim.diagnostic.config({
+        virtual_text = false
+      })
+    end
   }
 }
