ccd9f884b8fad31ec256525f2b55791aa62774f0
William Grosset <williamhgrosset@gmail.com>
2025-01-14 00:33:35 -0800

    Remove gitsigns and lsp_lines

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -109,14 +109,5 @@ return {
         )
       })
     end
-  },
-  {
-    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-    config = function()
-      require('lsp_lines').setup()
-      vim.diagnostic.config({
-        virtual_text = false
-      })
-    end
   }
 }
