c0d6fd552b2333a5df55d1ffac4a5b11ea3e1909
William Grosset <williamhgrosset@gmail.com>
2025-03-18 17:46:16 -0700

    Update git aliases and load secrets config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,12 +20,14 @@ alias zx "z"
 ## Git
 alias gs "git status"
 alias gd "git diff"
-alias gp "git push"
+alias gpsh "git push"
+alias gpll "git pull"
 alias gb "git branch"
 alias gch "git checkout"
 alias ga "git add"
 alias gall "git add --all"
 alias gc "git commit -m"
+alias gr "git restore"
 
 ## Python
 alias python "/usr/local/bin/python3"
@@ -46,3 +48,8 @@ set -Ux fish_user_paths $fish_user_paths ~/.deno/bin
 # Initialize tools
 starship init fish | source
 zoxide init fish | source
+
+# Load secrets
+if test -f ~/.config/fish/secrets.fish
+  source ~/.config/fish/secrets.fish
+end
